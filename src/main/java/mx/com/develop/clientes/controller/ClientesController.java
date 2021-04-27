a83259c9748e146ca8f86682f67eb44c43e4104e
FrankJMinor <pancrao.86@gmail.com>
2021-04-26 22:53:46 -0500

    [fix]: corrección archivo pom y main

--- src/main/java/mx/com/develop/clientes/controller/ClientesController.java
+++ src/main/java/mx/com/develop/clientes/controller/ClientesController.java
@@ -2,7 +2,6 @@ package mx.com.develop.clientes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class ClientesController {
--- src/main/java/mx/com/develop/clientes/dao/IClienteDAO.java
+++ src/main/java/mx/com/develop/clientes/dao/IClienteDAO.java
@@ -1,14 +1,7 @@
 package mx.com.develop.clientes.dao;
 
-import mx.com.develop.clientes.dto.ClienteDTO;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-
-import java.util.List;
 
 @Repository
 @Transactional(readOnly = true, rollbackFor = Exception.class)
